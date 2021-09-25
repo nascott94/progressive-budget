ef536f45e97d5a58b2221254aeeb55cdee0c2b76
nascott <nascott94@outlook.com>
2021-09-25 08:22:56 -0600

    adding service worke

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,12 @@
-const FILES_TO_CACHE = ["/", "index.html"];
+const FILES_TO_CACHE = [
+  "/",
+  "index.html",
+  "db.js",
+  "manifest.json",
+  "style.css",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png",
+];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
